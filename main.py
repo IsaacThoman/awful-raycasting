47005e2ed3e445bf1cdf91ee00134e27136cc4c2
isaacthoman <isaacthoman@outlook.com>
2021-08-17 17:34:30 -0700

    sorta 3D

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    rects = [pygame.Rect(70,70,50,50),pygame.Rect(150,250,130,20)]
+    rects = []
+    rects.append(pygame.Rect(70,70,50,50))
+    rects.append(pygame.Rect(0,330,350,20))
     angle+=1
     startX = 175
     startY = 175
@@ -21,10 +23,17 @@ while run:
     dumbStupid = -1
     for rectNum in rects:
         dumbStupid += 1
-        pygame.draw.rect(window,(0,255,255),rects[dumbStupid])
+        #pygame.draw.rect(window,(0,255,255),rects[dumbStupid])
     funnycolor = 0
-    for angleScanning in range(angle-20,angle+20):
-        angleScanning=angleScanning*2
+    renderRects = []
+    if angle >= 360:
+        angle = (angle % 360)
+    lineOn = 0
+    for angleScanning in range(angle-30, angle + 30):
+        lineOn +=1
+        #if angleScanning > 360:
+            #angleScanning = angleScanning % 360
+        angleScanning=angleScanning
         for i in range(350):
             breakMe = False
             myX = int(startX + i * math.cos(angleScanning * 3.1 / 180))
@@ -33,14 +42,20 @@ while run:
             for rectNum in rects:
                 dumbStupid +=1
                 if(myX>rects[dumbStupid].left and myX < rects[dumbStupid].right and myY > rects[dumbStupid].top and myY < rects[dumbStupid].bottom):
+                    height = 350-i
+                    over = angleScanning-angle
+                    renderRects.append(pygame.Rect(300-lineOn*4,200-height/2,4,height))
                     breakMe = True
                     break
             if(breakMe):
                 breakMe=False
                 break
 
-            window.set_at((myX, myY), (255, 0, 0))
-
+            #window.set_at((myX, myY), (255, 0, 0))
+    iterationB = -1
+    for rectNum in renderRects:
+        iterationB +=1
+        pygame.draw.rect(window,(0,255,0),renderRects[iterationB])
 
     pygame.display.flip()
 
